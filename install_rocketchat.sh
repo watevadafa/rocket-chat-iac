798ccf947f4aef06e1a31c2d61cedc7b2a480b1a
Tushar Sarang <tushar@theprocedure.in>
2023-01-04 13:35:51 +0300

    adds cli prompt before applying the output plan

--- install_rocketchat.sh
+++ install_rocketchat.sh
@@ -7,4 +7,6 @@ terraform get -update
 terraform plan -var-file=terraform.tfvars -out="./plans/main.tfplan" -target=module.rocketchat
 
 # Step 3: Apply the infrastructure plan
+read -e -p "Apply the Terraform plan? [Y/N] " YN
+[[ $YN == "n" || $YN == "N" || $YN == "" ]] && exit 0
 terraform apply "./plans/main.tfplan"
